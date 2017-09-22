71c2a8edcb6c9d296c9f6d1aad8570069d454c0c
Andika R <andika.ruspandi@gmail.com>
2017-09-22 22:34:04 +0800

    Comment unused routes

--- app/routes.js
+++ app/routes.js
@@ -13,23 +13,23 @@ let $routes = {};
 
 $routes.init = () => {
   //admin
-  app.get('/admin', admin.index);
+  //app.get('/admin', admin.index);
   app.post('/admin/file-upload/:room_id', fn.loggedIn, admin.fileUpload);
   
   //user
   app.get('/', fn.loggedIn, user.index);
   app.get('/room/:room_id', fn.loggedIn, user.room);
   app.all('/lobby', fn.loggedIn, user.lobby);
-  app.get('/slide/:room_id/:img', user.slide);
+  app.get('/slide/:room_id/:img', fn.loggedIn, user.slide);
   
   //auth
-  app.get('/gateway', auth.gateway);
-  app.post('/login', auth.login);
-  app.post('/register', auth.register);
-  app.get('/forgot-password', auth.forgotPassword);
-  app.get('/delete-account', auth.deleteAccountView);
-  app.post('/delete-account', auth.deleteAccount);
-  app.get('/logout', auth.logout);
+  app.get('/gateway', fn.checkLogged, auth.gateway);
+  app.post('/login', fn.checkLogged, auth.login);
+  app.post('/register', fn.checkLogged, auth.register);
+  //app.get('/forgot-password', fn.checkLogged, auth.forgotPassword);
+  app.get('/delete-account', fn.checkLogged, auth.deleteAccountView);
+  app.post('/delete-account', fn.checkLogged, auth.deleteAccount);
+  app.get('/logout', fn.loggedIn, auth.logout);
 
   //passport
   passport.serializeUser(cpass.serialize);
